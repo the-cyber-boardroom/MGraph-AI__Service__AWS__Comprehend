bb7b2d7f446bed643accc16a29778bf77792ca5d
Dinis Cruz <dinis.cruz@owasp.org>
2025-11-14 18:29:17 +0000

    updated code of lambda_handler

--- mgraph_ai_service_aws_comprehend/fast_api/lambda_handler.py
+++ mgraph_ai_service_aws_comprehend/fast_api/lambda_handler.py
@@ -1,14 +1,14 @@
 import os
 
+if os.getenv('AWS_REGION'):  # only execute if we are not running inside an AWS Lambda function
 
-if os.getenv('AWS_REGION'):                                                    # only execute if we are running inside an AWS Lambda function
+    from osbot_aws.aws.lambda_.boto3__lambda     import load_dependencies       # using the lightweight file (which only has the boto3 calls required to load_dependencies)
+    from mgraph_ai_service_aws_comprehend.config import LAMBDA_DEPENDENCIES__AWS__COMPREHEND
 
-    from osbot_aws.aws.lambda_.boto3__lambda                               import load_dependencies       # using the lightweight file (which only has the boto3 calls required to load_dependencies)
-    from mgraph_ai_service_aws_comprehend.config                           import LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS
 
-    load_dependencies(LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS)
+    load_dependencies(LAMBDA_DEPENDENCIES__AWS__COMPREHEND)
 
-    def clear_osbot_modules():                                                 # todo: add this to load_dependencies method, since after it runs we don't need the osbot_aws.aws.lambda_.boto3__lambda
+    def clear_osbot_modules():                            # todo: add this to load_dependencies method, since after it runs we don't need the osbot_aws.aws.lambda_.boto3__lambda
         import sys
         for module in list(sys.modules):
             if module.startswith('osbot_aws'):
@@ -16,7 +16,7 @@ if os.getenv('AWS_REGION'):                                                    #
 
     clear_osbot_modules()
 
-error   = None                                                                 # pin these variables
+error   = None          # pin these variables
 handler = None
 app     = None
 
@@ -28,7 +28,7 @@ try:
         handler = _.handler()
         app     = _.app()
 except Exception as exc:
-    if os.getenv("AWS_LAMBDA_FUNCTION_NAME") is None:                          # raise exception when not running inside a lambda function
+    if os.getenv("AWS_LAMBDA_FUNCTION_NAME") is None:       # raise exception when not running inside a lambda function
         raise
     error = (f"CRITICAL ERROR: Failed to start service with:\n\n"
              f"{type(exc).__name__}: {exc}")
