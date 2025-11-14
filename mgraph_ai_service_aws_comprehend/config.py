0242eea129f3180e81413939118205e80a6024ad
Dinis Cruz <dinis.cruz@owasp.org>
2025-11-14 18:47:17 +0000

    updated poetry.lock

--- mgraph_ai_service_aws_comprehend/config.py
+++ mgraph_ai_service_aws_comprehend/config.py
@@ -3,4 +3,4 @@ from mgraph_ai_service_aws_comprehend import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph AI Service AWS Comprehend"
 FAST_API__DESCRIPTION                    = "Text classification service based on AWS Comprehend "
-LAMBDA_DEPENDENCIES__AWS__COMPREHEND    = ['osbot-fast-api-serverless==v1.27.0']
+LAMBDA_DEPENDENCIES__AWS__COMPREHEND    = ['osbot-fast-api-serverless==v1.28.0']
