cc93b1aea0ce6892d348bf3c977c78282ed5bac1
Dinis Cruz <dinis.cruz@owasp.org>
2025-11-15 01:47:34 +0000

    added couple more tests

--- tests/integration/Integration_Helper__Html_Service.py
+++ tests/integration/Integration_Helper__Html_Service.py
@@ -121,7 +121,7 @@ class test_Integration_Helper__Html_Service(TestCase):
             assert len(_.url_to_hashes("https://news.bbc.co.uk"              )) > 310       # this value changes quite a bit
 
         with print_duration():
-            assert len(_.url_to_hashes("https://news.bbc.co.uk/sport"        )) == 418
+            assert len(_.url_to_hashes("https://news.bbc.co.uk/sport"        )) > 410
 
         with print_duration():
             assert len(_.url_to_hashes("https://www.bbc.co.uk/sport/football/articles/c5y0vz2eze7o")) == 180
@@ -130,6 +130,7 @@ class test_Integration_Helper__Html_Service(TestCase):
 
         with print_duration():
             assert len(_.url_to_hashes("https://text.npr.org/"               )) == 33
+
 
 
 SAMPLE__HTML__SIMPLE    = "<html><body>Hello World</body></html>"
--- tests/integration/test__int__Routes__Comprehend__Batch.py
+++ tests/integration/test__int__Routes__Comprehend__Batch.py
@@ -56,17 +56,34 @@ class test__int__Routes__Comprehend__Batch(TestCase):
     def test__process__hashes__google_com(self):
         sentiment = self.helper__detect_sentiment__from_url("https://www.google.com")
         assert len(sentiment)                    == 24
-        sentiment.print()
         assert sentiment['13348442cc'].sentiment == 'Neutral'
         assert sentiment['1834a46420'].sentiment == 'Neutral'
 
     # this needs splitting
-    # def test__process__hashes__docs_diniscruz_ai(self):
-    #     sentiment = self.helper__detect_sentiment__from_url("https://docs.diniscruz.ai")
-    #     assert len(sentiment)                    == 24
-    #     sentiment.print()
-    #     assert sentiment['13348442cc'].sentiment == 'Neutral'
-    #     assert sentiment['1834a46420'].sentiment == 'Neutral'
+    def test__process__hashes__docs_diniscruz_ai(self):
+        sentiment = self.helper__detect_sentiment__from_url("https://docs.diniscruz.ai")
+        assert len(sentiment)                    == 148
+        assert sentiment['02d0685572'].sentiment == 'Neutral'
+        assert sentiment['21fedf5eed'].sentiment == 'Positive'
+
+    def test__process__hashes__news_bbc_co_uk(self):
+        sentiment = self.helper__detect_sentiment__from_url("https://news.bbc.co.uk/")
+        assert len(sentiment)                    > 320
+        assert sentiment['003d2274a9'].sentiment == 'Neutral'
+        assert sentiment['02129bb861'].sentiment == 'Neutral'
+
+    def test__process__hashes__news_bbc_co_uk__sport(self):
+        sentiment = self.helper__detect_sentiment__from_url("https://news.bbc.co.uk/sport")
+        assert len(sentiment)                    > 410
+        assert sentiment['001857037b'].sentiment == 'Neutral'
+        assert sentiment['0102363dbd'].sentiment == 'Neutral'
+
+    def test__process__hashes__news_bbc_co_uk__sport(self):
+        sentiment = self.helper__detect_sentiment__from_url("https://news.bbc.co.uk/sport")
+        assert len(sentiment)                    > 410
+        sentiment.print()
+        assert sentiment['001857037b'].sentiment == 'Neutral'
+        assert sentiment['0102363dbd'].sentiment == 'Neutral'
 
 
 
